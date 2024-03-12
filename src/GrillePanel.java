0a070b77e5a03046b43510281d19ec80fc453b59
pernella-hub <125458656+pernella-hub@users.noreply.github.com>
2024-03-12 11:18:25 +0100

    GrillePanel.java

--- src/GrillePanel.java
+++ src/GrillePanel.java
@@ -7,7 +7,8 @@ import java.awt.event.ActionListener;
 public class GrillePanel extends JPanel {
     // Puissance 4 a 6 lignes
     private final int lignes = 6;
-    // 7 colonnes
+
+    // et 7 colonnes
     private final int colonnes = 7;
     // La grille de boutons
     private JButton[][] boutons;
@@ -24,24 +25,22 @@ public class GrillePanel extends JPanel {
                 JButton button = new JButton();
                 button.setActionCommand(Integer.toString(j));
                 button.addActionListener(actionListener);
-                // Ajout du bouton à la grille des boutons
                 boutons[i][j] = button;
-                // Ajout du bouton au panel
                 add(button);
             }
         }
+    }
 
-        // Appel du controleur pour mettre à jour l'affichage de la grille
-        public void miseAJourGrille;char[][] etatGrille; {
-            for (int i = 0; i < lignes; i++) {
-                for (int j = 0; j < colonnes; j++) {
-                    if (etatGrille[i][j] == 'X') {
-                        boutons[i][j].setText("X");
-                    } else if (etatGrille[i][j] == 'O') {
-                        boutons[i][j].setText("O");
-                    } else {
-                        boutons[i][j].setText("");
-                    }
+    // Appel du controleur pour mettre à jour l'affichage
+    public void miseAJourGrille(char[][] etatGrille) {
+        for (int i = 0; i < lignes; i++) {
+            for (int j = 0; j < colonnes; j++) {
+                if (etatGrille[i][j] == 'X') {
+                    boutons[i][j].setText("X");
+                } else if (etatGrille[i][j] == 'O') {
+                    boutons[i][j].setText("O");
+                } else {
+                    boutons[i][j].setText("");
                 }
             }
         }
